e77c7cc15213ecdfee43873c5817be93fd02e61f
webserverteam-a11y <webserverteam@ditechcdm.com>
2026-06-17 12:59:15 +0530

    Update navyug-contact-form-api.php

--- wordpress/navyug-contact-form-api.php
+++ wordpress/navyug-contact-form-api.php
@@ -31,12 +31,13 @@ add_action('admin_init', function () {
     register_setting('navyug_contact_form_settings', 'navyug_sendgrid_api_key', array(
         'type' => 'string',
         'sanitize_callback' => 'sanitize_text_field',
+        'default' => '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',
     ));
 
     register_setting('navyug_contact_form_settings', 'navyug_contact_to_email', array(
         'type' => 'string',
         'sanitize_callback' => 'sanitize_text_field',
-        'default' => 'navyugglobalventures@gmail.com,salvis.smglobal@gmail.com',
+        'default' => 'navyugglobalventures@gmail.com,dishan.m@ditechcdm.com,shubham.s@ditechcdm.com',
     ));
 
     register_setting('navyug_contact_form_settings', 'navyug_contact_from_email', array(
@@ -81,14 +82,15 @@ function navyug_contact_form_submit(WP_REST_Request $request) {
     $sendgrid_api_key = navyug_contact_form_config(
         'NAVYUG_SENDGRID_API_KEY',
         'NAVYUG_SENDGRID_API_KEY',
-        'navyug_sendgrid_api_key'
+        'navyug_sendgrid_api_key',
+        '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'
     );
     $to_emails = navyug_contact_form_recipients(
         navyug_contact_form_config(
             'NAVYUG_CONTACT_TO_EMAIL',
             'NAVYUG_CONTACT_TO_EMAIL',
             'navyug_contact_to_email',
-            'navyugglobalventures@gmail.com,salvis.smglobal@gmail.com'
+            'navyugglobalventures@gmail.com,dishan.m@ditechcdm.com,shubham.s@ditechcdm.com'
         )
     );
     $from_email = navyug_contact_form_config(
@@ -210,7 +212,7 @@ function navyug_contact_form_settings_page() {
         return;
     }
 
-    $default_recipients = 'navyugglobalventures@gmail.com,salvis.smglobal@gmail.com';
+    $default_recipients = 'navyugglobalventures@gmail.com,dishan.m@ditechcdm.com,shubham.s@ditechcdm.com';
     $default_from = 'navyugglobalventures@gmail.com';
     ?>
     <div class="wrap">
